d86a67f9534bb0a6ad9c757c432391d64e9666a7
mhgitrepo <mehedi1921@gmail.com>
2022-04-23 03:20:07 +0600

    Load Search result init-done

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
-const loadSearchResult = () => {
+const loadSearchResult = async () => {
     const searchResult = document.getElementById('search-field');
     const searchText = searchResult.value;
 
     // clear input field
     searchResult.value = '';
-    
-    console.log(searchText);
+
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data.data);
 };
