f792bff765a996207c187602a0f80f0862dd3b27
mhgitrepo <mehedi1921@gmail.com>
2022-04-23 03:46:10 +0600

    Displaying all data in viewport

--- main.js
+++ main.js
@@ -8,5 +8,25 @@ const loadSearchResult = async () => {
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data.data);
+    displayPhones(data.data);
+};
+
+const displayPhones = phones => {
+    const phonesContainer = document.getElementById('display-phones');
+    phonesContainer.textContent = ''
+
+    phones.forEach(phone => {
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card">
+            <img src="${phone.image}" class="card-img-top mx-auto mt-2 dp-img" alt="...">
+            <div class="card-body">
+            <h5 class="card-title"></h5>
+            <p class="card-text"></p>
+            </div>
+        </div>
+        `;
+        phonesContainer.appendChild(div);
+    });
 };
